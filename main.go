1982e49769ba37e1304584ef2153f02f1fb988df
affanhamid <affanhamid007@gmail.com>
2025-09-06 14:29:57 +0530

    added p2p structure

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/affanhamid/goDFS/p2p"
+)
 
 func main() {
-	fmt.Println("Go distributed file storage")
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
 }
